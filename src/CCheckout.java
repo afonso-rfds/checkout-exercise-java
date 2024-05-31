bd9dd3fb88384ee37fbc3cabbea66b166594b7ca
afonso-rfds <afonso.rfds@gmail.com>
2024-05-31 23:04:57 +0100

    Implemented CChouckout class

---
+++ src/CCheckout.java
@@ -0,0 +1,36 @@
+// File: CCheckout.java
+import java.util.ArrayList;
+import java.util.List;
+
+public class CCheckout {
+    private final List<CItem> m_items = new ArrayList<>();
+    private final List<CDiscount> m_promotions;
+
+    public CCheckout(List<CDiscount> f_promotions) {
+        this.m_promotions = f_promotions;
+    }
+
+    public void scanItem(CItem f_item) {
+        m_items.add(f_item);
+    }
+
+    public float calculatePriceWithoutDiscount() {
+        float priceWithoutDiscount = 0;
+
+        for (CItem item : m_items) {
+            priceWithoutDiscount += item.getPrice();
+        }
+
+        return priceWithoutDiscount;
+    }
+
+    public float calculateDiscount() {
+        float discount = 0;
+
+        for (CDiscount promotion : m_promotions) {
+            discount += promotion.getDiscount(m_items);
+        }
+
+        return discount;
+    }
+}
